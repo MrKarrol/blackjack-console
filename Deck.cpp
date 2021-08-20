fb1b10fe859507d12295c479f75d869945af5dd4
KrisKaroll <g.mishell@gmail.com>
2021-08-20 20:00:03 +0700

    Deck class added

--- Deck.cpp
+++ Deck.cpp
@@ -1,22 +1,65 @@
 #include "Deck.h"
 
+#include <random>
+#include <algorithm>
+
+#include "GenericPlayer.h"
+
 
 Deck::Deck()
 {
+	Populate();
+	Shuffle();
+	std::cout << m_cards.size() << std::endl;
+	for (const auto& card : m_cards)
+		std::cout << *card.get() << " ";
+	std::cout << std::endl;
+}
+
+template<typename T, typename ... Ts>
+void AddCards(T& hand, Ts ... ts)
+{
+	(hand.Add(ts, card::Suit::Clubs), ...);
+	(hand.Add(ts, card::Suit::Diamonds), ...);
+	(hand.Add(ts, card::Suit::Hearts), ...);
+	(hand.Add(ts, card::Suit::Spades), ...);
 }
 
 void Deck::Populate()
 {
+	using namespace card;
+	AddCards(*this, Rank::Ace, Rank::Two, Rank::Three, Rank::Four, Rank::Five,
+		Rank::Six, Rank::Seven, Rank::Eight, Rank::Nine, Rank::Ten,
+		Rank::Jack, Rank::Queen, Rank::King);
 }
 
 void Deck::Shuffle()
 {
+	std::random_device rd;
+	std::mt19937 g(rd());
+
+	std::shuffle(m_cards.begin(), m_cards.end(), g);
 }
 
-void Deck::Deal(Hand&)
+void Deck::Deal(Hand& hand)
 {
+	hand.Add(std::move(m_cards.back()));
+	m_cards.pop_back();
+	std::cout << m_cards.size() << std::endl;
+	for (const auto& card : m_cards)
+		std::cout << *card.get() << " ";
+	std::cout << std::endl;
 }
 
-void Deck::AdditionalCards(GenericPlayer&)
+void Deck::AdditionalCards(GenericPlayer& player)
 {
+	while (player.IsHitting())
+	{
+		Deal(player);
+		if (player.IsBoosted())
+		{
+			player.Bust();
+			return;
+		}
+	}
 }
--- blackjack-console.cpp
+++ blackjack-console.cpp
@@ -1,75 +1,11 @@
 #include "Player.h"
 #include "House.h"
+#include "Deck.h"
 
 
 int main()
 {
-    // test 1
-    {
-        std::cout << "----- test 1 -----" << std::endl;
-        {
-            Card card(card::Rank::Ace, card::Suit::Clubs, true);
-            std::cout << card << " ";
-            card.Flip();
-            std::cout << card << " ";
-        }
-        {
-            Card card(card::Rank::Jack, card::Suit::Diamonds);
-            std::cout << card << " ";
-        }
-        {
-            Card card(card::Rank::Ten, card::Suit::Spades);
-            std::cout << card << " ";
-        }
-        {
-            Card card(card::Rank::Queen, card::Suit::Hearts);
-            std::cout << card << " ";
-        }
-        {
-            Card card(card::Rank::King, card::Suit::Diamonds);
-            std::cout << card << std::endl;
-        }
-    }
-    // test 2
-    {
-        std::cout << "----- test 2 -----" << std::endl;
-        Player player("CoolJoker");
-        player.Add(card::Rank::Ace, card::Suit::Spades);
-        player.Add(card::Rank::Two, card::Suit::Hearts);
-        std::cout << player << std::endl;
-        if (player.IsHitting())
-        {
-            player.Add(card::Rank::Jack, card::Suit::Diamonds);
-            std::cout << player << std::endl;
-        }
-        player.Win();
-        player.Lose();
-        player.Push();
-    }
-    // test 3
-    {
-        std::cout << "----- test 3 -----" << std::endl;
-        House house;
-        house.Add(card::Rank::Ace, card::Suit::Spades, true);
-        house.Add(card::Rank::Two, card::Suit::Hearts);
-        std::cout << house << std::endl;
-        if (house.IsHitting())
-        {
-            house.Add(card::Rank::Jack, card::Suit::Diamonds);
-            std::cout << house << std::endl;
-        }
-        house.FlipFirstCard();
-        std::cout << house << std::endl;
-    }
-    // test 4
-    {
-        std::cout << "----- test 3 -----" << std::endl;
-        Player player("Loser");
-        player.Add(card::Rank::Ten, card::Suit::Hearts);
-        player.Add(card::Rank::Ten, card::Suit::Clubs);
-        player.Add(card::Rank::Ten, card::Suit::Diamonds);
-        if (player.GetValue() > 21)
-            player.Bust();
-    }
-
+    Deck deck;
+    Player player("Mike");
+    deck.AdditionalCards(player);
 }
